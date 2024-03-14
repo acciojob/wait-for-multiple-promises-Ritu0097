74be9ccfcc376af8fc479357c68032d86e4c0a90
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-03-14 16:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-//your JS code here. If required.
+const promise1 = () => {
+    return new Promise((resolve) => {
+        const randomTime = Math.floor(Math.random() * 3000)+1000; 
+        setTimeout(() => {
+            resolve({name:'Promise 1',time:randomTime/1000 });
+        },randomTime);
+    });
+}
+const promise2 = () => {
+    return new Promise((resolve) => {
+        const randomTime = Math.floor(Math.random() * 3000) + 1000;
+        setTimeout(() => {
+            resolve({name:'Promise 2', time: randomTime / 1000 });
+        }, randomTime);
+    });
+}
+const promise3 = () => {
+    return new Promise((resolve) => {
+        const randomTime = Math.floor(Math.random() * 3000) + 1000; 
+        setTimeout(() => {
+            resolve({name:'Promise 3', time: randomTime / 1000 });
+        }, randomTime);
+    });
+}
+const promises = [promise1(), promise2(), promise3()];
+
+Promise.all(promises)
+    .then((results) => {
+        const output = document.getElementById('output');
+        const loadingRow = document.getElementById('loading-row');
+        output.removeChild(loadingRow);
+
+        let totalTime = 0;
+        results.forEach((result) => {
+            const { name, time } = result;
+            totalTime += time;
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${name}</td>
+                <td>${time.toFixed(3)}</td>
+            `;
+            output.appendChild(row);
+        });
+
+        const totalRow = document.createElement('tr');
+        totalRow.innerHTML = `
+            <td>Total</td>
+            <td>${totalTime.toFixed(3)}</td>
+        `;
+        output.appendChild(totalRow);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
